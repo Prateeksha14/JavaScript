f15e7d747680c22d1a073c4cceef2999c97c159d
Prateeksha14 <155909681+Prateeksha14@users.noreply.github.com>
2024-01-11 10:49:38 +0000

    commit

--- Basic_js/02_datatype.js
+++ Basic_js/02_datatype.js
@@ -1,1 +1,24 @@
 console.log("hello");
+
+ let name = "prateeksha"; // string
+ let age = 18; // integer
+ let isLoggedIn = "false"; // boolean
+
+//  number => 2 to power 53
+// bigint => used for larger number
+// string => used in double and single quotes
+// boolean => true or false
+// null => standalone value
+// undefined => when value is no define
+// symbol => unquie
+
+// these are primative datatypes
+
+
+// OBJECTS
+
+console.log(typeof("pratiksha")); // typeof used for knowing datatype => String
+
+console.log(typeof(null));// typeof used for knowing datatype it give => Object
+
+console.log(typeof(undefined));// typeof used for knowing datatype it give => undefined
